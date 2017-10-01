9c1ecbcd0acb9ba7f7e555ebf6ed3b68433fd5d9
DateBack <caskooss@gmail.com>
2017-10-01 21:47:53 +0300

    Test

--- seminars/canvas/script.js
+++ seminars/canvas/script.js
@@ -1,4 +1,17 @@
-var canvas = document.getElementById("sandbox"); 
-var context = canvas.getContext("2d"); 
+var canvas = document.getElementById("sandbox");
+var context = canvas.getContext("2d");
+var circle, square; 
 
-context.fillRect(50, 25, 200, 250);
+square = new Path2D(); 
+square.moveTo(50, 50); 
+square.lineTo(250, 50); 
+square.lineTo(250, 250); 
+square.lineTo(50, 250); 
+square.lineTo(50, 50); 
+
+context.stroke(square); 
+
+circle = new Path2D();
+circle.arc(150, 150, 100, 0, 2 * Math.PI); 
+
+context.fill(circle);
