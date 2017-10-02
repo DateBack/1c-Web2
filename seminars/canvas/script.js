cd3797ebc5951010bc17c59ba63cef5f76bdd238
DateBack <caskooss@gmail.com>
2017-10-02 23:57:35 +0300

    /

--- seminars/canvas/script.js
+++ seminars/canvas/script.js
@@ -1,17 +1,29 @@
 var canvas = document.getElementById("sandbox");
-var context = canvas.getContext("2d");
-var circle, square; 
+var context = canvas.getContext("2d"); 
 
-square = new Path2D(); 
-square.moveTo(50, 50); 
-square.lineTo(250, 50); 
-square.lineTo(250, 250); 
-square.lineTo(50, 250); 
-square.lineTo(50, 50); 
-
-context.stroke(square); 
+var line, circle; 
 
 circle = new Path2D();
-circle.arc(150, 150, 100, 0, 2 * Math.PI); 
+circle.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, 0,2 * Math.PI); 
+context.stroke(circle); 
 
-context.fill(circle);
+line = new Path2D(); 
+
+
+var R = canvas.width / 2, d, angle, pX, pY, qX, qY; 
+var d, angle, pX, pY, qX, qY; 
+for (d = 0; d < 60; ++d) {
+    angel = (d / 60) * (2 * Math.PI); 
+    pX = Math.cos(angel) * R; 
+    pY = -Math.sin(angel) * R; 
+    qX = 0,9 * pX; 
+    qY= 0.9 * pY; 
+    pX+=R; pY+=R; 
+    qX+=R; qY+=R; 
+
+    line.moveTo(pX, pY);
+    line.lineTo(qX, qY); 
+    
+    context.stroke(line);
+    
+}
