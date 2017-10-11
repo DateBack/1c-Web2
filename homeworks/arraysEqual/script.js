06196f15f4849001b6037588fdaac226cec7129a
DateBack <caskooss@gmail.com>
2017-10-11 18:35:37 +0300

    .

--- homeworks/arraysEqual/script.js
+++ homeworks/arraysEqual/script.js
@@ -1,6 +1,6 @@
 function areArraysEqual(A, B) {
     for (var i = 0; i < A.length; ++i) {
-       if (A[i] != B[i]) return false;
+       if (A[i] !== B[i]) return false;
     } 
     return true;
 }
